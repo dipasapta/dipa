a1289dd3a45558882023fc34386f779db700a4e3
dipasapta <67253171+dipasapta@users.noreply.github.com>
2020-11-14 00:23:39 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,1 @@
+console. clearlog('baccha');
---
+++ javascript.js
@@ -0,0 +1,14 @@
+function validation() {
+	var name = document.getElementById("name").value;
+
+
+	var email = document.getElementById("email").value;
+	var phone = document.getElementById("phone").value;
+	
+
+
+	alert("Enter Your valide Name");
+
+	return false;
+	
+}
